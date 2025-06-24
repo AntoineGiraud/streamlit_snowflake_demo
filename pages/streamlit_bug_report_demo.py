353b5c880a87df6e347f9dc0d4a2c0d4baf21202
AntoineGiraud <antoine.giraud@outlook.com>
2025-06-24 09:39:14 +0200

    refactor: prevent SQL Injection from ❄️ demo

--- pages/streamlit_bug_report_demo.py
+++ pages/streamlit_bug_report_demo.py
@@ -1,44 +1,40 @@
 import streamlit as st
 
-session = st.connection('snowflake').session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-# Change the query to point to your table
+st.title("👻 Bug report demo!")
+st.info("Demo given by [snowflake - Streamlit Getting Started](https://docs.snowflake.com/en/developer-guide/streamlit/getting-started#build-your-first-sis-app)", icon="ℹ️")
+
+
 def get_data(_session):
+    """Fetch table data"""
     query = """
-    select * from bikeshare.bronze.BUG_REPORT_DATA
-    order by date desc
-    limit 100
+        select * from bikeshare.bronze.BUG_REPORT_DATA
+        order by date desc
+        limit 100
     """
     data = _session.sql(query).collect()
     return data
 
-# Change the query to point to your table
-def add_row_to_db(session, row):
-    sql = f"""INSERT INTO bikeshare.bronze.BUG_REPORT_DATA VALUES
-    ('{row['author']}',
-    '{row['bug_type']}',
-    '{row['comment']}',
-    '{row['date']}',
-    '{row['bug_severity']}')"""
 
-    session.sql(sql).collect()
+def add_row_to_db(cnx, row):
+    """Safely insert a row into the BUG_REPORT_DATA table using parameterized queries."""
+    # cf. doc [st.connections.snowflakeconnection](docs.streamlit.io/develop/api-reference/connections/st.connections.snowflakeconnection#snowflakeconnectioncursor)
+    sql = """
+        INSERT INTO bikeshare.bronze.BUG_REPORT_DATA (author, bug_type, comment, date, bug_severity)
+        VALUES (?, ?, ?, ?, ?)
+    """
+    params = (row["author"], row["bug_type"], row["comment"], row["date"], row["bug_severity"])
+    cnx.cursor().execute(sql, params)
 
-# st.set_page_config(page_title="Bug report", layout="centered")
-
-st.title("Bug report demo!")
-
-# st.sidebar.write(
-#     f"This app demos how to read and write data from a Snowflake Table"
-# )
 
 form = st.form(key="annotation", clear_on_submit=True)
 
 with form:
     cols = st.columns((1, 1))
     author = cols[0].text_input("Report author:")
-    bug_type = cols[1].selectbox(
-        "Bug type:", ["Front-end", "Back-end", "Data related", "404"], index=2
-    )
+    bug_type = cols[1].selectbox("Bug type:", ["Front-end", "Back-end", "Data related", "404"], index=2)
     comment = st.text_area("Comment:")
     cols = st.columns(2)
     date = cols[0].date_input("Bug date occurrence:")
@@ -47,14 +43,7 @@ with form:
 
 if submitted:
     try:
-        add_row_to_db(
-            session,
-            {'author':author,
-            'bug_type': bug_type,
-            'comment':comment,
-            'date':str(date),
-            'bug_severity':bug_severity
-        })
+        add_row_to_db(cnx, {"author": author, "bug_type": bug_type, "comment": comment, "date": str(date), "bug_severity": bug_severity})
         st.success("Thanks! Your bug was recorded in the database.")
         st.balloons()
     except Exception as e:
