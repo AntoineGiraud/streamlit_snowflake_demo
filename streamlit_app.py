d80dd77746757b47d6b13f8343a12d67d863739b
AntoineGiraud <antoine.giraud@outlook.com>
2025-06-24 09:34:43 +0200

    feat: try bug report demo from snowflake
    https://docs.snowflake.com/en/developer-guide/streamlit/getting-started#build-your-first-sis-app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 # Define the pages
 main_page = st.Page("pages/streamlit_home.py", title="Accueil", icon="🎈")
+page_1 = st.Page("pages/streamlit_bug_report_demo.py", title="Bug report demo", icon="👻")
 page_2 = st.Page("pages/streamlit_load_csv.py", title="Load table from .csv", icon="🔀")
 
 # Set up navigation
-pg = st.navigation([main_page, page_2])
+pg = st.navigation([main_page, page_1, page_2])
 
 # Sidebar pour les filtres généraux
 st.sidebar.markdown("Youpi ❄️")
